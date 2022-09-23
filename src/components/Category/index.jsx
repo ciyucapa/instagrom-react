003e84074e83dc598649a38e3ef65adc86d93239
ciyucapa <cindycaceres134@gmail.com>
2022-09-23 08:46:08 -0500

    [FEAT]: Add readme!

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 const Category = ({src, alt, href}) => (
-    <a href={href} >
+    <a href={href}>
         <img src={src} alt={alt} />
     </a>
 );
